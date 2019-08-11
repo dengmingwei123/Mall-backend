675dc51df4c9b5a1e3d28b21cf1851c07e513d82
dengmingwei123 <1311827579@qq.com>
2019-08-11 22:20:50 +0800

    用户登录框完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,36 @@
 <template>
-  <div>
-    登录页
+  <div class='login'>
+    <!-- 登录框 -->
+    <el-form
+      :model="loginForm"
+      label-width="70px"
+      :rules="rules"
+    >
+      <!-- 登录头像 -->
+      <img
+        src="../assets/images/诗乃.jpg"
+        alt=""
+      >
+      <!-- 用户名 -->
+      <el-form-item
+        label="用户名"
+        prop="username"
+      >
+        <el-input v-model='loginForm.username'></el-input>
+      </el-form-item>
+      <!-- 密码 -->
+      <el-form-item
+        label="密码"
+        prop="password"
+      >
+        <el-input v-model="loginForm.password"></el-input>
+      </el-form-item>
+      <!-- 登录按钮 -->
+      <el-form-item>
+        <el-button type="primary">登录</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -8,11 +38,46 @@
 export default {
   data() {
     return {
-
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '用户名不能为空', trigger: 'change' },
+          { min: 3, max: 9, message: '必须是在 3 - 9 位字符', trigger: 'change' }
+        ]
+      }
     }
   }
 }
 </script>
 
 <style lang="less" spaced>
+.login {
+  height: 100%;
+  background-color: #2d434c;
+  overflow: hidden;
+  .el-form {
+    width: 400px;
+    margin: 250px auto;
+    background-color: #fff;
+    padding: 75px 40px 15px 40px;
+    border-radius: 20px;
+    position: relative;
+    img {
+      width: 140px;
+      height: 140px;
+      position: absolute;
+      left: 50%;
+      top: 0;
+      transform: translate(-50%, -90px);
+      border-radius: 50%;
+      border: 10px solid #fff;
+    }
+    .el-button + .el-button {
+      margin-left: 100px;
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
+// 引入vue
 import Vue from 'vue'
+// 引入element-ui
+import ElementUI from 'element-ui'
+// 引入element-ui的css文件
+import 'element-ui/lib/theme-chalk/index.css'
+// 引入根组件
 import App from './App'
+// 引入路由
 import router from './router'
+// 引入通用样式
+import '@/assets/common.css'
 
 // 引入axios
 import axios from 'axios'
+// 使用element-ui
+Vue.use(ElementUI)
 
 // 设置axios的默认值
 axios.default.baseURL = 'http://localhost:8888/api/private/v1/'
